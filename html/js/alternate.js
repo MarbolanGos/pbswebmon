43f411aa105ffa600eb48c96da000754706169d9
MarbolanGos <marbolangos@gmail.com>
2012-03-02 11:26:00 +0100

    Rearrange js

--- html/js/alternate.js
+++ html/js/alternate.js
@@ -1,14 +1,10 @@
 function alternate(id){
- if(document.getElementsByTagName){  
-   var table = document.getElementById(id);  
-   var rows = table.getElementsByTagName("tr");  
-   for(i = 0; i < rows.length; i++){          
- //manipulate rows
-     if(i % 2 == 0){
-       rows[i].className = "even";
-     }else{
-       rows[i].className = "odd";
-     }      
-   }
- }
+	if(document.getElementsByTagName){
+		var table = document.getElementById(id);
+		var rows = table.getElementsByTagName("tr");
+		for(i = 0; i < rows.length; i++){
+			//manipulate rows
+			rows[i].className = (i % 2 == 0) ? rows[i].className = "even" : rows[i].className = "odd";
+		}
+	}
 }
